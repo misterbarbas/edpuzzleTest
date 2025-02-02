3f341865c894d42567e38ea18a3838b3c099630a
Samuel Sansó <samuelsanso7@hotmail.es>
2025-02-03 00:47:12 +0100

    stack pro modified

--- lib/blog-infrastructure-stack.ts
+++ lib/blog-infrastructure-stack.ts
@@ -4,10 +4,21 @@ import * as rds from 'aws-cdk-lib/aws-rds';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as elb from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import * as elasticloadbalancingv2targets from "aws-cdk-lib/aws-elasticloadbalancingv2-targets";
 import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as cloudwatch_actions from 'aws-cdk-lib/aws-cloudwatch-actions';
+//import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as cloudwatch from 'aws-cdk-lib/aws-cloudwatch';
 import { Construct } from 'constructs';
+//import { Instance } from '@aws-cdk/aws-ec2';
+//import { Instance } from '@aws-cdk/aws-cloudwatch';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+//import * as ec2 from '@aws-cdk/aws-ec2';
+
+
+//This a stack for production
 
 export class BlogInfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -45,6 +56,8 @@ export class BlogInfrastructureStack extends cdk.Stack {
       //publiclyAccessible: false,
       //databaseName: 'blogDB',
     //});
+
+
       // Defining s3 bucket for static content
 
       const staticAssetsBucket = new s3.Bucket(this, 'StaticAssetsBucket', {
@@ -61,6 +74,13 @@ export class BlogInfrastructureStack extends cdk.Stack {
           behaviors: [{ isDefaultBehavior: true }],
         }],
       });
+
+      //Defining certificates
+
+      //const certificate = new acm.Certificate(this, 'BlogCertificate', {
+        //domainName: 'edblog.com',
+        //validation: acm.CertificateValidation.fromDns(hostedZone),
+      //});
       
       //An application loadbalancer to distribute the workloads
 
@@ -68,10 +88,40 @@ export class BlogInfrastructureStack extends cdk.Stack {
         vpc,
         internetFacing: true,
       });
-      
-      // To configure the domain of the website
 
-      const hostedZone = route53.HostedZone.fromLookup(this, 'HostedZone', {
+      const Listener = lb.addListener('Listener', {
+         port: 80
+         //certificates: [certificate],
+        
+        });;
+        const targetGroup = new elb.ApplicationTargetGroup(this, 'BlogTargetGroup', {
+          vpc,
+          targetType: elb.TargetType.INSTANCE, // Target type is EC2 instances
+          port: 80, // Port on which targets are listening
+        });
+
+         // addint tg to load balancer
+         targetGroup.addTarget(new elasticloadbalancingv2targets.InstanceTarget(ec2Instance));
+
+
+
+        // adding tg to the listener
+        Listener.addTargetGroups('DefaultTargetGroup', {
+          targetGroups: [targetGroup], // Add the target group to the listener
+        });
+        //Another way to define it
+      //const targetGroup = Listener.addTargets('ec2Instance',
+        // { port: 80 });
+      
+      // Allow HTTP access
+      ec2Instance.connections.allowFrom(lb, ec2.Port.tcp(80));
+
+      // Allow SSH access
+      ec2Instance.connections.allowFrom(lb, ec2.Port.tcp(22));
+
+       // To configure the domain of the website
+
+       const hostedZone = route53.HostedZone.fromLookup(this, 'HostedZone', {
         domainName: 'edblog.com',
       });
       
@@ -80,9 +130,32 @@ export class BlogInfrastructureStack extends cdk.Stack {
         recordName: 'www',
         target: route53.RecordTarget.fromAlias(new targets.LoadBalancerTarget(lb)),
       });
-      
 
+     //Monitoring to fix
+
+     // Define the CPU Utilization Metric for the EC2 instance
+    //const cpuUtilizationMetric = ec2.Instance.metricCpuUtilization();
+
+    // Create a CloudWatch Alarm based on the CPU Utilization metric
+    //const cpuAlarm = new cloudwatch.Alarm(this, 'CPUUtilizationAlarm', {
+      //metric: cpuUtilizationMetric,
+      //threshold: 80,  // Trigger the alarm if CPU usage exceeds 80%
+      //evaluationPeriods: 2, // Number of periods to evaluate the metric
+      //comparisonOperator: cloudwatch.ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+      //alarmDescription: 'This alarm is triggered if CPU utilization exceeds 80%.',
+    //});
     
+    
+    //Sending notification
+
+    // Create an SNS topic
+const alarmTopic = new sns.Topic(this, 'CpuUtilizationAlarmTopic');
+
+// Add the SNS topic to the CloudWatch alarm
+//cpuAlarm.addAlarmAction(new cloudwatch_actions.SnsAction(alarmTopic));
+
+// You can also add an email subscription to the SNS topic
+alarmTopic.addSubscription(new cdk.aws_sns_subscriptions.EmailSubscription('samuelsanso7@hotmail.es'));
     
   }
 }
